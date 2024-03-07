7d43b72b868bc240b061bf29d139f8a3b71fadf9
AishiD <97897398+AishiD@users.noreply.github.com>
2024-03-07 19:54:13 +0530

    should've done before

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import "./App.css";
 import Header from "./components/Header";
-import Title from "./components/Title";
 import Home from "./components/Pages/Home";
+import Academics from './components/Pages/Academics';
+import Profile from './components/Pages/Profile';
+import Skills from './components/Pages/Skills';
+import Projects from './components/Pages/Projects';
+import Publications from "./components/Pages/Publications";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
       <div>
         <Header />
-        <Title />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/publications" element={<Publications />} />
+            <Route path="/academics" element={<Academics />} />
+          </Routes>
+        </div>
       </div>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,46 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { HomeIcon, UserIcon, WrenchScrewdriverIcon, AcademicCapIcon, BookOpenIcon, CodeBracketIcon } from "@heroicons/react/24/outline";
-import Home from '../components/Pages/Home';
+import { Link, useLocation } from "react-router-dom";
+import {
+  HomeIcon,
+  UserIcon,
+  WrenchScrewdriverIcon,
+  AcademicCapIcon,
+  BookOpenIcon,
+  CodeBracketIcon,
+} from "@heroicons/react/24/outline";
+import Home from "../components/Pages/Home";
 
 export default function Header() {
   return (
-    <div className="flex justify-center mt-5" >
+    <div className="flex justify-center mt-5">
       <nav className=" relative flex flex-row items-center gap-6 border-2 border-purple-700 rounded-full px-4 py-2 m-2 w-fit hover:scale-105 transition-transform ">
-        <HomeIcon color="#7E22CE" className="h-6 w-6 hover:bg-purple-100 hover:rounded-lg hover:scale-105 transition-transform" ><a href={Home} className="flex items-center hover:text-black transition-colors">
-          Pages
-        </a></HomeIcon>
-        <UserIcon color="#7E22CE" className="h-6 w-6 hover:bg-purple-100 hover:rounded-lg hover:scale-105 transition-transform" />
-        <WrenchScrewdriverIcon color="#7E22CE" className="h-6 w-6 hover:bg-purple-100 hover:rounded-lg hover:scale-105 transition-transform" />
-        <CodeBracketIcon color="#7E22CE" className="h-6 w-6 hover:bg-purple-100 hover:rounded-lg hover:scale-105 transition-transform"/>
-        <BookOpenIcon color="#7E22CE" className="h-6 w-6 hover:bg-purple-100 hover:rounded-lg hover:scale-105 transition-transform"/>
-        <AcademicCapIcon color="#7E22CE" className="h-6 w-6 hover:bg-purple-100 hover:rounded-lg hover:scale-105 transition-transform"/>
+        <Link to="/">
+          <HomeIcon
+            color="#7E22CE"
+            className="h-6 w-6 hover:bg-purple-100 hover:rounded-lg hover:scale-105 transition-transform"
+          ></HomeIcon>
+        </Link>
+        <Link to='/profile'><UserIcon
+          color="#7E22CE"
+          className="h-6 w-6 hover:bg-purple-100 hover:rounded-lg hover:scale-105 transition-transform"
+        /></Link>
+        <Link to='/skills'><WrenchScrewdriverIcon
+          color="#7E22CE"
+          className="h-6 w-6 hover:bg-purple-100 hover:rounded-lg hover:scale-105 transition-transform"
+        /></Link>
+        <Link to='/projects'><CodeBracketIcon
+          color="#7E22CE"
+          className="h-6 w-6 hover:bg-purple-100 hover:rounded-lg hover:scale-105 transition-transform"
+        /></Link>
+        <Link to='/publications'><BookOpenIcon
+          color="#7E22CE"
+          className="h-6 w-6 hover:bg-purple-100 hover:rounded-lg hover:scale-105 transition-transform"
+        /></Link>
+        <Link to='/academics'><AcademicCapIcon
+          color="#7E22CE"
+          className="h-6 w-6 hover:bg-purple-100 hover:rounded-lg hover:scale-105 transition-transform"
+        /></Link>
       </nav>
     </div>
   );
