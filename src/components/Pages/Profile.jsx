23beb95cca53ba953c09bfcd9cff0b7edafa1994
AishiD <97897398+AishiD@users.noreply.github.com>
2024-06-11 17:12:12 +0530

    added image in profile

--- src/components/Pages/Profile.jsx
+++ src/components/Pages/Profile.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from 'react';
+import "../../../public/pp_photo.png";
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div className='flex flex-col sm:flex-row md:flex-row lg:flex justify-evenly py-4 px-20 h-fit items-center'>
+      <div className='w- items-center'><img src="pp_photo.png" className='w-96 h-96 border'/></div>
+      <div className='w-1/2 bg-blue-100 text-xl'><h1>I am a front-end web developer and UI/UX Designer based in Kolkata</h1></div>
+    </div>
   )
 }
 
