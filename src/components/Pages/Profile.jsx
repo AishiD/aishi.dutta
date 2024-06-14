11ee8b2c2f2a719bd56f49eaec5137634d6e41d5
AishiD <97897398+AishiD@users.noreply.github.com>
2024-06-14 18:14:50 +0530

    profile complete

--- src/components/Pages/Profile.jsx
+++ src/components/Pages/Profile.jsx
@@ -1,46 +1,100 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import "../../../public/pp_photo.png";
+import ppPhoto from "../../../public/pp_photo.png";
+import {
+  MapPinIcon,
+  LanguageIcon,
+  GlobeAltIcon,
+} from "@heroicons/react/16/solid";
+import { motion } from "framer-motion";
+
+const sleep = async (ms) =>
+  await new Promise((resolve) => setTimeout(resolve, ms));
+
+function Img({ delay }) {
+  return (
+    <motion.div
+      initial="initial"
+      animate="upDown"  // Example: animate to "rotate" on mount
+      whileHover="grow"
+      variants={{
+        grow: {
+          scale: 1.1,
+        },
+        upDown: {
+          y: [-12, 12], // Move up and down
+          transition: {
+            duration: 2,
+            repeat: Infinity,
+            repeatType: "reverse",
+          }},
+        initial: {
+          y: [-20, 20],
+          rotate: 0,
+          transition: {
+            delay,
+            duration: 2,
+            repeat: Infinity,
+            repeatType: "reverse",
+          },
+        },
+      }}
+      >
+      <img src={ppPhoto} alt="Profile" className="w-fit h-fit sm:w-96 sm:h-96 md:w-96 md:h-96" />
+    </motion.div>
+  );
+}
 
 function Profile() {
   return (
     <div className="flex flex-col sm:flex-row md:flex-row lg:flex justify-center gap-10 py-10 px-10 h-screen align-top">
       {/* this is profile image */}
       <div className="">
-        <img
-          src="pp_photo.png"
-          className="w-fit h-fit sm:w-96 sm:h-96 md:w-96 md:h-96"
-        />
+      <Img delay={0.5} />
       </div>
       {/* this is profile text */}
-      <div className="w-full md:w-1/2 sm:w-1/2 text-2xl overflow-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
-        <h1 className="sm:text-5xl md:text-5xl text-bold">
-          I am a <span className="text-purple-700">front-end web developer</span> and <span className="text-purple-700">UI/UX Designer</span> based in Kolkata
+      <div className="flex flex-col w-full md:w-1/2 sm:w-1/2 overflow-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+        <h1 className="text-3xl sm:text-5xl md:text-5xl pb-6 md:pb-10 lg:pb-10">
+          I am a <span className="text-purple-700">final-year CSE </span>student
+          at <span className="text-purple-700">IEM, Kolkata</span>
         </h1>
-        <h1>
-          I am a front-end web developer and UI/UX Designer based in Kolkata
-        </h1>
-        <h1>
-          I am a front-end web developer and UI/UX Designer based in Kolkata
-        </h1>
-        <h1>
-          I am a front-end web developer and UI/UX Designer based in Kolkata
-        </h1>
-        <h1>
-          I am a front-end web developer and UI/UX Designer based in Kolkata
-        </h1>
-        <h1>
-          I am a front-end web developer and UI/UX Designer based in Kolkata
-        </h1>
-        <h1>
-          I am a front-end web developer and UI/UX Designer based in Kolkata
-        </h1>
-        <h1>
-          I am a front-end web developer and UI/UX Designer based in Kolkata
-        </h1>
-        <h1>
-          I am a front-end web developer and UI/UX Designer based in Kolkata
-        </h1>
+        <div className="flex flex-col md:flex-row lg:flex-row gap-8">
+          {/* location */}
+          <div className="flex flex-row gap-0 items-center">
+            <MapPinIcon className="w-10 h-10" color="red" />
+            <span className="font-bold text-lg  lg:text-xl">Kolkata</span>
+          </div>
+          {/* language */}
+          <div className="flex flex-row gap-0 items-center">
+            <LanguageIcon className="w-10 h-10" color="black" />
+            <span className="font-bold text-lg  lg:text-xl">English</span>
+          </div>
+          {/* gender */}
+          <div className="flex flex-row gap-0 items-center">
+            <GlobeAltIcon className="w-8 h-8" color="black" />
+            <span className="font-bold text-lg  lg:text-xl">Indian</span>
+          </div>
+        </div>
+        <p className="text-lg lg:text-xl leading-normal lg:leading-relaxed font-medium py-10">
+          A CSE Undergrad Who Is Curious About{" "}
+          <span className="text-purple-700 font-bold">Tech </span>And
+          <span className="text-yellow-500 font-bold">
+            {" "}
+            Everything Creative.
+          </span>{" "}
+          Being a{" "}
+          <span className="text-purple-700 font-bold">
+            front-end developer and UI/UX Designer
+          </span>{" "}
+          I Am Constantly Seeking Opportunities To Learn And Grow. I am a{" "}
+          <span className="text-yellow-500">
+            three-time gold medal winner in art
+          </span>
+          . I a driven individual ans can use my creative pursuits to bring out
+          the most in my workplace. Right Now I Am Pursuing My Undergraduate
+          Degree From In Computer Science And Engineering (Artificial
+          Intelligence).
+        </p>
       </div>
     </div>
   );
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -16,7 +16,10 @@ function Title() {
       <div className=" flex flex-col justify-center px-5 ">
         <div className="text-xl sm:text-2xl">My name is</div>
         <div>
-          <img src="name.png" className="w-96 h-32 sm:h-48 md:h-40 md:w-96 sm:w-96 lg:w-96" />
+          <img
+            src="name.png"
+            className="w-96 h-32 sm:h-48 md:h-40 md:w-96 sm:w-96 lg:w-96"
+          />
         </div>
         <div className="text-xl sm:text-2xl">
           I am passionate about Tech and{" "}
@@ -32,24 +35,27 @@ function Title() {
           <SocialIcon url="https://www.discord.com" bgColor="#7E22CE" />
         </div>
         <button className="h-fit w-fit px-10 py-2 relative inline-flex items-center justify-start overflow-hidden transition-all bg-white rounded hover:bg-white group border-solid border-2 border-purple-700">
-        {/* <button className="w-fit h-fit px-10 py-2 bg-purple-600 border-none text-xl z-10 relative transition-all duration-300 ease-in-out rounded-lg  hover:h-fit hover:rounded-l-md  hover:scale-110 hover:text-indigo-600 hover:border-2 hover:border-indigo-600"> */}
-        <span className="w-0 h-0 rounded bg-purple-600 absolute top-0 left-0 ease-out duration-500 transition-all group-hover:w-full group-hover:h-full -z-1"></span>
-  <span className="w-full text-purple-700 transition-colors duration-300 ease-in-out group-hover:text-white z-10">
-    <a href="https://drive.google.com/file/d/1cPJFzjKCnASd6BRVpkE2wod_xlJGs7H7/view?usp=sharing">Resume</a>
-  </span>
- </button>
-
+          {/* <button className="w-fit h-fit px-10 py-2 bg-purple-600 border-none text-xl z-10 relative transition-all duration-300 ease-in-out rounded-lg  hover:h-fit hover:rounded-l-md  hover:scale-110 hover:text-indigo-600 hover:border-2 hover:border-indigo-600"> */}
+          <span className="w-0 h-0 rounded bg-purple-600 absolute top-0 left-0 ease-out duration-300 transition-all group-hover:w-full group-hover:h-full -z-1"></span>
+          <span className="w-full text-purple-700 transition-colors duration-300 ease-in-out group-hover:text-white z-10">
+            <a href="https://drive.google.com/file/d/1cPJFzjKCnASd6BRVpkE2wod_xlJGs7H7/view?usp=sharing">
+              Resume
+            </a>
+          </span>
+        </button>
       </div>
       {/* this is the infinite scrolling section */}
       <div className="hidden sm:block md:block h-screen px-10 overflow-y-hidden">
-      <Marquee
-    fade={true}
-    direction='up'
-    reverse={false}
-    pauseOnHover={false}
-    className="h-screen"
-    innerClassName="repeat"
-  ><img src="scroll_img.png" className=""/></Marquee>
+        <Marquee
+          fade={true}
+          direction="up"
+          reverse={false}
+          pauseOnHover={false}
+          className="h-screen"
+          innerClassName="repeat"
+        >
+          <img src="scroll_img.png" className="" />
+        </Marquee>
       </div>
     </div>
   );
