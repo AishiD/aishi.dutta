d8337a59cfa28ca987f1aec06b60f1d20c600b7e
AishiD <97897398+AishiD@users.noreply.github.com>
2024-06-15 12:41:33 +0530

    skills started

--- src/components/Pages/Skills.jsx
+++ src/components/Pages/Skills.jsx
@@ -1,9 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { SocialIcon } from 'react-social-icons';
+
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <div className='flex flex-col justify-start gap-10 py-10 px-10 h-screen align-top'>
+      {/* heading */}
+      <div className='font-bold font-mono text-xl md:text-3xl lg:text-3xl text-purple-600'>Skills</div>
+      {/* languages */}
+      <div className='flex flex-row'>
+        <SocialIcon url='www.html5.com' />
+      </div>
+      {/* graphic design */}
+      <div></div>
+    </div>
   )
 }
 
