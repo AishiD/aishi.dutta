6a50e72868468febddf1b8ac80f62070326fee10
AishiD <97897398+AishiD@users.noreply.github.com>
2024-06-16 21:07:41 +0530

    skills full complete

--- src/components/Tech_Stack.jsx
+++ src/components/Tech_Stack.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 function TechStack({ img, name }) {
     return (
-        <div className="flex flex-col flex-shrink-0 tech-stack items-center w-24 md:w-24 lg:w-32 p-2 bg-white rounded-lg shadow-md transform transition-transform duration-300 ease-in-out hover:scale-110">
+        <div className="flex flex-col flex-shrink-0 tech-stack items-center w-32 md:w-24 lg:w-32 p-2 bg-white rounded-lg shadow-md transform transition-transform duration-300 ease-in-out hover:scale-110">
             <img src={img} alt={name} className="w-full h-auto object-contain" />
             <h4 className="text-center mt-2 text-sm md:text-base lg:text-lg">{name}</h4>
         </div>
