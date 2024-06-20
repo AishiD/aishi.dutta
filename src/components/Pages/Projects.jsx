dce6477dada8d6bf5d0ab2294618b9415f722b8e
AishiD <97897398+AishiD@users.noreply.github.com>
2024-06-20 13:46:05 +0530

    projects section complete

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import pblog_img from "../../assets/pblog_img.png";
 import aqualynk_img from "../../assets/aqualynk_img.png";
 import GDwork from "../GDwork.tsx";
+import app_img from "../../assets/app_img.png"
+import medsy from "../../assets/medsy.png"
+import classy from "../../assets/classy.png"
 
 function Projects() {
   return (
@@ -18,7 +21,7 @@ function Projects() {
       </div>
       <div className="flex flex-col justify-center md:flex-row lg:flex-row gap-3 md:gap-5 lg:gap-8 ">
         {/* personal blog */}
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow ">
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md transform transition-transform duration-300 ease-in-out hover:scale-110 ">
           <a href="https://aishi-dutta-personal-blog.vercel.app/">
             <img className="rounded-t-lg object-cover" src={pblog_img} alt="" />
           </a>
@@ -29,8 +32,7 @@ function Projects() {
               </h5>
             </a>
             <p className="mb-3 font-normal text-gray-700 ">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
+            A personal blog website sharing my experiences and insights through various written blogs.
             </p>
             <div className="flex flex-row gap-2">
               <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -46,7 +48,7 @@ function Projects() {
           </div>
         </div>
         {/* Aqualynk */}
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow ">
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md transform transition-transform duration-300 ease-in-out hover:scale-110 ">
           <a href="https://astounding-fox-b995e8.netlify.app/">
             <img
               className="rounded-t-lg object-cover"
@@ -61,8 +63,7 @@ function Projects() {
               </h5>
             </a>
             <p className="mb-3 font-normal text-gray-700 ">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
+            An IoT pond water monitoring system ensures optimal conditions for effective fish breeding management.
             </p>
             <div className="flex flex-row gap-2">
               <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -78,7 +79,7 @@ function Projects() {
           </div>
         </div>
         {/* Threads app */}
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow ">
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md transform transition-transform duration-300 ease-in-out hover:scale-110 ">
           <a href="https://astounding-fox-b995e8.netlify.app/">
             <img
               className="rounded-t-lg object-cover"
@@ -93,8 +94,7 @@ function Projects() {
               </h5>
             </a>
             <p className="mb-3 font-normal text-gray-700 ">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
+            A Threads clone app for seamless real-time messaging and community engagement.
             </p>
             <div className="flex flex-row gap-2">
               <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -119,6 +119,100 @@ function Projects() {
         <GDwork className="flex flex-end"/>
       </div>
       {/* ui works */}
+      <div></div><div className="font-mono text-yellow-600 md:text-xl lg:text-xl">
+        UI/UX
+      </div>
+      <div className="flex flex-col justify-center md:flex-row lg:flex-row gap-3 md:gap-5 lg:gap-8 ">
+        {/* Language Learning App */}
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md transform transition-transform duration-300 ease-in-out hover:scale-110 ">
+          <a href="https://www.figma.com/design/2LKTQNax37GO1zSxHd03Jc/Language-App?node-id=0-1&t=caVhc54TBq23HDNt-1">
+            <img className="rounded-t-lg object-cover" src={app_img} alt="" />
+          </a>
+          <div className="p-5">
+            <a href="https://www.figma.com/design/2LKTQNax37GO1zSxHd03Jc/Language-App?node-id=0-1&t=caVhc54TBq23HDNt-1">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-purple-700 ">
+                Language Learning App
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 ">
+            Interactive platform facilitating language acquisition through immersive exercises and cultural insights worldwide.
+            </p>
+            <div className="flex flex-row gap-2">
+              <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Figma
+              </div>
+              <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                App
+              </div>
+              <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                UI
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Classy */}
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md transform transition-transform duration-300 ease-in-out hover:scale-110 ">
+          <a href="https://www.figma.com/design/Xmq9JzsmaCVDBWsRDxrMj3/Untitled?node-id=0-1&t=vhuLFvgllftbVnZm-1">
+            <img
+              className="rounded-t-lg object-cover"
+              src={classy}
+              alt=""
+            />
+          </a>
+          <div className="p-5">
+            <a href="https://www.figma.com/design/Xmq9JzsmaCVDBWsRDxrMj3/Untitled?node-id=0-1&t=vhuLFvgllftbVnZm-1">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-purple-700 ">
+                EdVerse
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 ">
+            Interactive digital platform enhances learning, engagement, and collaboration among students and educators.
+            </p>
+            <div className="flex flex-row gap-2">
+              <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Figma
+              </div>
+              <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                WebApp
+              </div>
+              <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Education
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Medsy */}
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md transform transition-transform duration-300 ease-in-out hover:scale-110">
+          <a href="https://www.figma.com/community/file/1266436091567606897/medsy">
+            <img
+              className="rounded-t-lg object-cover"
+              src={medsy}
+              alt=""
+            />
+          </a>
+          <div className="p-5">
+            <a href="https://www.figma.com/community/file/1266436091567606897/medsy">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-purple-700 ">
+                Medsy
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 ">
+            Integrated healthcare system prioritizing patient needs through personalized care and efficient medical services.
+            </p>
+            <div className="flex flex-row gap-2">
+              <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Figma
+              </div>
+              <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Medicine
+              </div>
+              <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                WebApp
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
