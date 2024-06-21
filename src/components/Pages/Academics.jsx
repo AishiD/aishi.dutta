7e7ed4758131f3efaffce3ea5528aba8f5268fc2
AishiD <97897398+AishiD@users.noreply.github.com>
2024-06-21 12:51:17 +0530

    education started

--- src/components/Pages/Academics.jsx
+++ src/components/Pages/Academics.jsx
@@ -1,9 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import TimelineEd from '../TimelineEd'
+import TimelineEx from '../TimelineEx'
 
 function Academics() {
   return (
-    <div>Academics</div>
+    <div className='bg-purple-100 flex flex-col justify-center w-full px-10'>
+      <div className='text-xl lg:text-3xl text-purple-700 font-bold'>Education</div>
+      <TimelineEd />
+      <div className='text-xl lg:text-3xl text-purple-700 font-bold'>Experience</div>
+      <TimelineEx />
+    </div>
   )
 }
 
