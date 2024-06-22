23224b03f108355a4468ef041ba1667f51132813
AishiD <97897398+AishiD@users.noreply.github.com>
2024-06-22 19:59:10 +0530

    academics done

--- src/components/Pages/Academics.jsx
+++ src/components/Pages/Academics.jsx
@@ -5,7 +5,7 @@ import TimelineEx from '../TimelineEx'
 
 function Academics() {
   return (
-    <div className='bg-purple-100 flex flex-col justify-center w-full px-10'>
+    <div className='flex flex-col justify-center w-full px-10 py-10'>
       <div className='text-xl lg:text-3xl text-purple-700 font-bold'>Education</div>
       <TimelineEd />
       <div className='text-xl lg:text-3xl text-purple-700 font-bold'>Experience</div>
--- src/components/TimelineEx.jsx
+++ src/components/TimelineEx.jsx
@@ -1,41 +1,70 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import { WalletIcon,StarIcon } from '@heroicons/react/24/outline';
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import {
+  BuildingOffice2Icon,
+  ChartBarSquareIcon, BuildingOfficeIcon
+} from "@heroicons/react/24/outline";
 
 function TimelineEx() {
   return (
-    <div><VerticalTimeline>
-    <VerticalTimelineElement
-      className="vertical-timeline-element--work"
-      contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-      contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-      date="2011 - present"
-      iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-      icon={<WalletIcon />}
-    >
-      <h3 className="vertical-timeline-element-title">Creative Director</h3>
-      <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-      <p>
-        Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-      </p>
-    </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    date="2002 - 2006"
-    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-    icon={<StarIcon />}
-  >
-    <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-    <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-    <p>
-      Creative Direction, Visual Design
-    </p>
-  </VerticalTimelineElement>
-    
-  </VerticalTimeline></div>
-  )
+    <div>
+      <VerticalTimeline lineColor="purple">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(137, 46, 255)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(137, 46, 255)" }}
+          date="Aug,2023 - May,2024"
+          iconStyle={{ background: "rgb(137, 46, 255)", color: "#fff" }}
+          icon={<BuildingOffice2Icon />}
+        >
+          <h3 className="vertical-timeline-element-title">SDE Intern</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            IEMA Research & Development Pvt. Ltd.
+          </h4>
+          <p>
+            Worked on real-life projects focusing on front-end development and
+            User experience.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          contentStyle={{background: 'rgb(239, 222, 255)'}}
+          date="2022 - present"
+          iconStyle={{ background: "rgb(137, 46, 255)", color: "#fff" }}
+          icon={<ChartBarSquareIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Team Lead of Art & Design
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            IIC(Institute's Innovation Council), IEM
+          </h4>
+          <p>Creative Direction, Event Organisation</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(137, 46, 255)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(137, 46, 255)" }}
+          date="May,2023 - Jul,2024"
+          iconStyle={{ background: "rgb(137, 46, 255)", color: "#fff" }}
+          icon={<BuildingOfficeIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">AI ML Virtual Intern</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            AICTE NEAT
+          </h4>
+          <p>
+            Worked on real-life projects and a vividly learning experience focusing on AWS Servies.
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
-export default TimelineEx
+export default TimelineEx;
