a4c38c41c96c64b72832d04576e37370374ae556
AishiD <97897398+AishiD@users.noreply.github.com>
2024-06-26 12:08:16 +0530

    v1

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -34,9 +34,9 @@ function Title() {
           <SocialIcon url="https://github.com/AishiD" bgColor="#7E22CE" />
           <SocialIcon url="https://www.discord.com" bgColor="#7E22CE" />
         </div>
-        <button className="h-fit w-fit px-10 py-2 relative inline-flex items-center justify-start overflow-hidden transition-all bg-white rounded hover:bg-white group border-solid border-2 border-purple-700">
+        <button className="h-fit w-fit px-10 py-2 relative inline-flex items-center justify-start overflow-hidden transition-all bg-white rounded-xl hover:bg-white group border-solid border-2 border-purple-700">
           {/* <button className="w-fit h-fit px-10 py-2 bg-purple-600 border-none text-xl z-10 relative transition-all duration-300 ease-in-out rounded-lg  hover:h-fit hover:rounded-l-md  hover:scale-110 hover:text-indigo-600 hover:border-2 hover:border-indigo-600"> */}
-          <span className="w-0 h-0 rounded bg-purple-600 absolute top-0 left-0 ease-out duration-300 transition-all group-hover:w-full group-hover:h-full -z-1"></span>
+          <span className="w-0 h-0 rounded-lg bg-purple-600 absolute top-0 left-0 ease-out duration-300 transition-all group-hover:w-full group-hover:h-full -z-1"></span>
           <span className="w-full text-purple-700 transition-colors duration-300 ease-in-out group-hover:text-white z-10">
             <a href="https://drive.google.com/file/d/1cPJFzjKCnASd6BRVpkE2wod_xlJGs7H7/view?usp=sharing">
               Resume
